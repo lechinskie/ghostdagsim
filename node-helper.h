d78267b4c9dc077960090c481776c1a23a8e3fb3
eramoss <eramossl@outlook.com>
2026-01-22 20:20:04 -0300

    Node helper to install de NodeContainer into the network

---
+++ node-helper.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include "dag.h"
+
+#include "ns3/application-container.h"
+#include "ns3/ipv4-address.h"
+#include "ns3/node-container.h"
+#include "ns3/object-factory.h"
+#include "ns3/uinteger.h"
+
+namespace ns3
+{
+
+class GhostDagNodeHelper
+{
+  public:
+    GhostDagNodeHelper(Address address,
+                       std::vector<Ipv4Address>& peers,
+                       std::map<Ipv4Address, double>& peers_download_speeds,
+                       std::map<Ipv4Address, double>& peers_upload_speeds,
+                       NodeInternetSpeeds& internetSpeeds,
+                       NodeStats* stats);
+
+    GhostDagNodeHelper();
+
+    void commonConstructor(Address address,
+                           std::vector<Ipv4Address>& peers,
+                           std::map<Ipv4Address, double>& peers_download_speeds,
+                           std::map<Ipv4Address, double>& peers_upload_speeds,
+                           NodeInternetSpeeds& internet_speeds,
+                           NodeStats* stats);
+
+    void SetAttribute(std::string name, const AttributeValue& value);
+
+    ApplicationContainer Install(NodeContainer c);
+    ApplicationContainer Install(Ptr<Node> node);
+    ApplicationContainer Install(std::string node_name);
+
+    void SetPeersAddresses(std::vector<Ipv4Address>& peers_addresses);
+    void SetPeersDownloadSpeeds(std::map<Ipv4Address, double>& peers_download_speeds);
+    void SetPeersUploadSpeeds(std::map<Ipv4Address, double>& peers_upload_speeds);
+    void SetNodeInternetSpeeds(NodeInternetSpeeds& internetSpeeds);
+    void SetNodeStats(NodeStats* node_stats);
+
+  protected:
+    virtual Ptr<Application> InstallPriv(Ptr<Node> node);
+
+    ObjectFactory m_factory;
+    std::string m_protocol;
+    Address m_address;
+    std::vector<Ipv4Address> m_peers_addresses;
+    std::map<Ipv4Address, double> m_peers_download_speeds;
+    std::map<Ipv4Address, double> m_peers_upload_speeds;
+    NodeInternetSpeeds m_internet_speeds;
+    NodeStats* m_node_stats;
+};
+
+} // namespace ns3
